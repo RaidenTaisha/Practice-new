fbe4997b4dd22df234769d6279628b715c57c4a1
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-07-06 00:50:01 +0700

    воваблин
    воваблин

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
-
+#include <String>
 #include <QApplication>
-
+using namespace std;
 int main(int argc, char *argv[])
 {
     int awerwerwerwer=10;
     int awemaria=20;
+    string strrr = "Воваблять";
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
