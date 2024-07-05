b841297a5def43dbf993105e6c53b05f16947a33
Бажен <95105368+Tam15937@users.noreply.github.com>
2024-07-06 01:12:15 +0700

    sdfhh
    sdfgh

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include "mainwindow.h"
-
+#include <string>
 #include <QApplication>
-
+using namespace std;
 int main(int argc, char *argv[])
 {
     int awerwerwerwer=10;
     int awemaria=20;
     int awemaria2=30;
      int awemariasdad3=30;
+    string str = "Баженблять";
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
